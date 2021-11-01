37aca7705f324e1bca04df580696e962b5fae14c
mehmetafsar510 <drmehmet510@gmail.com>
2021-11-01 18:14:29 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -20,10 +20,10 @@ server {
   server_name {FullDomainName};
 
   location / {
-    proxy_set_header        Host $host:$server_port;
-    proxy_set_header        X-Real-IP $remote_addr;
-    proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_set_header        X-Forwarded-Proto $scheme;
+    proxy_set_header        Host \$host:\$server_port;
+    proxy_set_header        X-Real-IP \$remote_addr;
+    proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header        X-Forwarded-Proto \$scheme;
     proxy_pass              http://jenkins;
     # Required for new HTTP-based CLI
     proxy_http_version 1.1;
@@ -45,10 +45,10 @@ server {
   server_name {FullDomainName};
 
   location / {
-    proxy_set_header        Host $host:$server_port;
-    proxy_set_header        X-Real-IP $remote_addr;
-    proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_set_header        X-Forwarded-Proto $scheme;
+    proxy_set_header        Host \$host:\$server_port;
+    proxy_set_header        X-Real-IP \$remote_addr;
+    proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header        X-Forwarded-Proto \$scheme;
     proxy_pass              http://jenkins;
     # Required for new HTTP-based CLI
     proxy_http_version 1.1;
