c53140b7752705bc85d4ed6d48160617d3ca16ba
mehmetafsar510 <drmehmet510@gmail.com>
2021-11-01 10:58:57 +0300

    update

--- init.sql
+++ init.sql
@@ -1,11 +1,11 @@
-CREATE TABLE phonebook_db.phonebook(
+CREATE TABLE phonebook.phonebook(
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     number VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO phonebook_db.phonebook (name, number)
+INSERT INTO phonebook.phonebook (name, number)
     VALUES
         ("Callahan", "1234567890"),
         ("Sergio Taco", "67854"),
