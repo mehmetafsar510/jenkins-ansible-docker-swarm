3c2ad00bcf67fe934d083f6f7b6eb128f5793a79
mehmetafsar510 <drmehmet510@gmail.com>
2021-11-01 14:49:04 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -2,31 +2,31 @@
 
 amazon-linux-extras install epel -y
 yum -y install nginx vim
-yum -y install certbot python2-certbot-ngi
+yum -y install certbot python2-certbot-nginx
 
 sh -c "cat >> /etc/nginx/conf.d/jenkins.conf<< 'EOF'
 ################################################
 # Nginx Proxy configuration
 #################################################
 upstream jenkins {
-  server {SERVERIP}:8000 fail_timeout=0;
+  server 54.221.80.35:8000 fail_timeout=0;
 }
 server {
   listen 80;
-  server_name {FullDomainName};
+  server_name phonebook.mehmetafsar.net;
 
   location / {
-    proxy_set_header        Host $host:$server_port;
-    proxy_set_header        X-Real-IP $remote_addr;
-    proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_set_header        X-Forwarded-Proto $scheme;
+    proxy_set_header        Host \$host:\$server_port;
+    proxy_set_header        X-Real-IP \$remote_addr;
+    proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header        X-Forwarded-Proto \$scheme;
     proxy_pass              http://jenkins;
     # Required for new HTTP-based CLI
     proxy_http_version 1.1;
     proxy_request_buffering off;
     proxy_buffering off; # Required for HTTP-based CLI to work over SSL
   }
-}             
+}
 EOF"
 
 systemctl enable --now nginx
